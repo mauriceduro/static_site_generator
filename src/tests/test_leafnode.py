2c46278c1d7928479d9c56d8d604ae21f44a60cd
Maurice Duro <mauriceduro@outlook.com>
2025-09-22 11:45:04 +0100

    Add Leaf Node class and associated tests

---
+++ src/tests/test_leafnode.py
@@ -0,0 +1,18 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, World!")
+        result = "<p>Hello, World!</p>"
+        self.assertEqual(node.to_html(), result)
+     
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Hello, World!", {"href":"https//youtube.com", "target": "_blank"})
+        result = "<a href:\"https//youtube.com\" target:\"_blank\">Hello, World!</a>"
+        self.assertEqual(node.to_html(), result)
+        
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, World!")
+        result = "<b>Hello, World!</b>"
+        self.assertEqual(node.to_html(), result)
