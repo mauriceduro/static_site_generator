3d067268473d94d4812387878e23b640e8782b9a
Maurice Duro <mauriceduro@outlook.com>
2025-09-22 11:13:41 +0100

    Add HTML Node class and associated tests

---
+++ src/tests/test_htmlnode.py
@@ -0,0 +1,17 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_equal(self):
+        node = HTMLNode("p", "This is the value.", [], {"href":"https//youtube.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), " href:\"https//youtube.com\" target:\"_blank\"")
+
+    def test_props_to_html_not_equal(self):
+        node = HTMLNode("p", "This is the value.", [], {"href":"https//youtube.com", "target": "_blank"})
+        self.assertNotEqual(node.props_to_html(), " href:\"https//google.com\" target:\"_blank\"")
+
+    def test_props_to_html_is_none(self):
+        node = HTMLNode("p", "This is the value.")
+        self.assertEqual(node.props_to_html(), "") 
+
+   
