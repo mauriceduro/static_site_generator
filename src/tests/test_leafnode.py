88e567046cbc6ded6d32bdb2632a43624ba4c263
Maurice Duro <mauriceduro@outlook.com>
2025-09-22 17:15:18 +0100

    Fix: Update href to include '=' not ':'

--- src/tests/test_leafnode.py
+++ src/tests/test_leafnode.py
@@ -9,10 +9,13 @@ class TestLeafNode(unittest.TestCase):
      
     def test_leaf_to_html_a(self):
         node = LeafNode("a", "Hello, World!", {"href":"https//youtube.com", "target": "_blank"})
-        result = "<a href:\"https//youtube.com\" target:\"_blank\">Hello, World!</a>"
+        result = "<a href=\"https//youtube.com\" target=\"_blank\">Hello, World!</a>"
         self.assertEqual(node.to_html(), result)
         
     def test_leaf_to_html_b(self):
         node = LeafNode("b", "Hello, World!")
         result = "<b>Hello, World!</b>"
         self.assertEqual(node.to_html(), result)
+
+if __name__ == "__main__":
+    unittest.main()
